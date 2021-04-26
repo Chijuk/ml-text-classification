bb9440a46dcceadfad6e0da1c8bcc1f400112561
evgeniy.chumak <evgeniy.chumak@omniway.ua>
2021-04-26 11:45:15 +0300

    refactor(preprocessor): Settings module refactor

--- src/preprocess.py
+++ src/preprocess.py
@@ -8,17 +8,14 @@ from flashtext import KeywordProcessor
 import logger_utils
 import stop_words_utils
 import text_utils
-from settings import CleanerSetting, get_setting, SettingType
+from settings import CleanerSetting, get_setting, SettingType, BaseSetting
 
 log = logging.getLogger("preprocess")
 
 
 @logger_utils.profile
 def data_preprocess(setting: CleanerSetting, stop_words: KeywordProcessor) -> None:
-    input_data_path = os.getcwd() + setting.input_data_path
-    log.debug(f'Reading data from: {input_data_path}')
-    data = pd.read_csv(input_data_path, header=0, delimiter=";", encoding='utf-8')
-    log.info("Data shape: {}".format(data.shape))
+    data = __read_data(setting)
 
     log.info("Start text cleaning")
     data["text"] = data["text"].apply(text_utils.clean_text, args=(setting, stop_words))
@@ -32,11 +29,19 @@ def data_preprocess(setting: CleanerSetting, stop_words: KeywordProcessor) -> No
     log.info(f'Saved to: {output_data_path}')
 
 
+def __read_data(setting: BaseSetting) -> pd.DataFrame:
+    input_data_path = os.getcwd() + setting.input_data_path
+    log.debug(f'Reading data from: {input_data_path}')
+    data = pd.read_csv(input_data_path, header=0, delimiter=";", encoding='utf-8')
+    log.info("Data shape: {}".format(data.shape))
+    return data
+
+
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("Please provide cleaner settings path. Exit...")
     elif len(sys.argv) == 2:
-        cleaner_settings = get_setting(str(sys.argv[1]), SettingType.cleaner)
+        cleaner_settings: CleanerSetting = get_setting(str(sys.argv[1]), SettingType.cleaner)
         logger_utils.init_logging(cleaner_settings.log_path + "\\" + cleaner_settings.name)
         log.info("==> Start initialization")
         stop_words_processor = stop_words_utils.load_processor(cleaner_settings)
