bc3dddb94a7e0969d002cd46105ddb2f7de5da9e
evgeniy.chumak <evgeniy.chumak@omniway.ua>
2021-05-26 20:16:57 +0300

    feat(general): added setup.py
    Closes #12

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='machine-learning',
+    version='1.0.0',
+    packages=['machine-learning', 'service', 'preprocessor', 'utils', 'bin', 'stop_words'],
+    package_dir={
+        'machine-learning': 'app',
+        'bin': 'resources\\bin',
+        'stop_words': 'resources\\stop_words',
+        'service': 'app\\service',
+        'preprocessor': 'app\\preprocessor',
+        'utils': 'app\\utils'
+    },
+    package_data={
+        'bin': ['*.bin'],
+        'stop_words': ['*.txt'],
+    },
+    url='https://omniway.ua',
+    license='Omniway',
+    author='Evgeniy Chumak',
+    author_email='evgeniy.chumak@omniway.ua',
+    description='Machine learning tools',
+    python_requires='==3.8',
+    platform='win-amd64'
+)
